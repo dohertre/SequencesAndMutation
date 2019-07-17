2c03a208f634bf5cde00c5f06c76a1b07ec0598a
dohertre <dohertre@rose-hulman.edu>
2019-07-17 18:13:10 -0400

    random

--- src/m6_mutation.py
+++ src/m6_mutation.py
@@ -200,7 +200,6 @@ def MUTATE_replace_negatives_by_zeros(numbers):
         if numbers[k] < 0:
             numbers[k] = 0
     #return Numbers
-    return None
 # ----------------------------------------------------------------------
 # If this module is running at the top level (as opposed to being
 # imported by another module), then call the 'main' function.
